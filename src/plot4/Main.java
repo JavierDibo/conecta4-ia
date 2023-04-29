5eed907ed1bb435805117ee9d75e3411332556f3
Javier Francisco Dibo Gomez <jfdg0002@red.ujaen.es>
2023-04-29 14:47:40 +0200

    Cambiado Main para que tenga una seleccion correcta en el menu de jugadores

--- src/plot4/Main.java
+++ src/plot4/Main.java
@@ -171,19 +171,14 @@ public class Main extends JFrame implements ActionListener {
         if (jugadorcpu) {
             switch (iaplayer) {
                 case MINIMAX:
-                    /*
                     player2 = new MiniMaxPlayer();
                     System.out.println("CPU (MiniMax)");
                     cabecera = "Juego: Humano vs CPU MiniMax - ";
-                    */
-                    player2 = new GreedyPlayer();
-                    System.out.println("CPU (Greedy)");
-                    cabecera = "Juego: Humano vs CPU Greedy - ";
                     break;
                 case MINIMAXRES:
-                    player2 = new MiniMaxPlayer();
-                    System.out.println("CPU (MiniMax)");
-                    cabecera = "Juego: Humano vs CPU MiniMax - ";
+                    player2 = new MiniMaxRestrainedPlayer();
+                    System.out.println("CPU (MiniMax Restringido)");
+                    cabecera = "Juego: Humano vs CPU MiniMax Restringido - ";
                     break;
                 case ALFABETA:
                     player2 = new AlfaBetaPlayer();
@@ -191,14 +186,9 @@ public class Main extends JFrame implements ActionListener {
                     cabecera = "Juego: Humano vs CPU MiniMax AlfaBeta - ";
                     break;
                 default:
-                    player2 = new MiniMaxRestrainedPlayer();
-                    System.out.println("CPU (MiniMax Restringido)");
-                    cabecera = "Juego: Humano vs CPU MiniMax Restringido - ";
-                    /*
                     player2 = new GreedyPlayer();
                     System.out.println("CPU (Greedy)");
                     cabecera = "Juego: Humano vs CPU Greedy - ";
-                    */
             }
         } else {
             player2 = null;
